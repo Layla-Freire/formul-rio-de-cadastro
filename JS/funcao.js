82cc68c83d062ec1dc4deb59c2ae1990b0b2cb5b
Layla Freire <94554030+Layla-Freire@users.noreply.github.com>
2022-05-17 00:07:33 -0300

    commit
    atualização das funções

--- JS/funcao.js
+++ JS/funcao.js
@@ -1,19 +1,5 @@
-// VALIDAÇÃO DO FORMULÁRIO
+
 'use strict';
-(function() {
-  window.addEventListener('load', function() {
-    var forms = document.getElementsByClassName('needs-validation');
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})
 
 //CONSUMO DE API DO CEP
 
@@ -82,6 +68,5 @@ function enviar(){
      
   };
   console.log(formValue);
-  alert("Dados enviados com Sucesso!");
 }
  
